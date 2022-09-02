c25be2daa7fa24e4995fd46d9ab73cab04da385e
takashi otsuka <shoshi100010000@gmail.com>
2022-09-02 23:45:27 +0900

    add sort function (order by desc)

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe 'タスク管理機能', type: :system do
         # expect(page).to have_content 'Task'
         # expect(page).to have_content 'Task_Failure'
 
-        fill_in "Name", with: "task"
-        fill_in "Content", with: "content"
+        fill_in "タスクの名前", with: "task"
+        fill_in "内容", with: "content"
         click_on "登録する"
         
         
@@ -20,22 +20,35 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
   end
   describe '一覧表示機能' do
+    let!(:task) do
+      task = FactoryBot.create(:task)
+      task = FactoryBot.create(:task)
+      task = FactoryBot.create(:task)
+    end
+    before do
+        visit tasks_path
+    end
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task)
-        task = FactoryBot.create(:task)
-        task = FactoryBot.create(:task)
-        visit tasks_path
         for n in 1..3
           expect(page).to have_content "task#{n}"
           expect(page).to have_content "content#{n}"
         end
       end
     end
+
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task_list = all('.task_row') 
+        p task
+        p task_list
+      end
+    end
+
   end
   describe '詳細表示機能' do
-     context '任意のタスク詳細画面に遷移した場合' do
-       it '該当タスクの内容が表示される' do
+    context '任意のタスク詳細画面に遷移した場合' do
+      it '該当タスクの内容が表示される' do
         
         task=FactoryBot.create(:task)
         visit task_path(task.id)
@@ -43,7 +56,7 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content "task"
         expect(page).to have_content "content"
         
-       end
-     end
+      end
+    end
   end
 end
