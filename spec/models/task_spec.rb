fca0600808e1ab47c4165bdd66032bff45b648b2
takashi otsuka <shoshi100010000@gmail.com>
2022-09-13 17:26:53 +0900

    fix system spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -30,29 +30,30 @@ describe 'タスクモデル機能', type: :model do
   end
   describe '検索機能' do
     
-    let!(:first_task) { FactoryBot.create(:first_task) }
-    let!(:second_task) { FactoryBot.create(:second_task)}
-    let!(:third_task) { FactoryBot.create(:third_task)}
+    let!(:task1) { FactoryBot.create(:task1) }
+    let!(:task2) { FactoryBot.create(:task2)}
+    let!(:task3) { FactoryBot.create(:task3)}
     context 'scopeメソッドでタイトルのあいまい検索をした場合' do
       it "検索キーワードを含むタスクが絞り込まれる" do
-        expect(Task.name_search('task')).to include(first_task)
-        expect(Task.name_search('task')).not_to include(second_task)
-        expect(Task.name_search('task').count).to eq 2
+        expect(Task.name_search('task1')).to include(task1)
+        expect(Task.name_search('task1')).not_to include(task2)
+        expect(Task.name_search('task1').count).to eq 1
       end
     end
     context 'scopeメソッドでステータス検索をした場合' do
       it "ステータスに完全一致するタスクが絞り込まれる" do
-        expect(Task.status_search('未着手')).to include(second_task)
-        expect(Task.status_search('未着手')).not_to include(first_task)
-        expect(Task.status_search('未着手').count).to eq 2
+        expect(Task.status_search('着手中')).to include(task1)
+        expect(Task.status_search('着手中')).not_to include(task2)
+        expect(Task.status_search('着手中').count).to eq 1
         
       end
     end
     context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
       it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
-        expect(Task.name_search('task').status_search('着手中')).to include(first_task)
-        expect(Task.name_search('task').status_search('着手中')).not_to include(second_task)
-        expect(Task.name_search('task').status_search('着手中').count).to eq 1
+        expect(Task.name_search('task2').status_search('未着手')).to include(task2)
+        expect(Task.name_search('task2').status_search('未着手')).not_to include(task1)
+        expect(Task.name_search('task2').status_search('未着手')).not_to include(task3)
+        expect(Task.name_search('task2').status_search('未着手').count).to eq 1
       end
     end
   end
